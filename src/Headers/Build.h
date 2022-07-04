6d59c80a943d76aad349038cd5ccfa3675b7d823
aiekick <aiekick@funparadigm.com>
2022-07-05 01:21:05 +0200

    [FIX] : fix the Deferred Renderer by adding diffuse and specular component         its seems the shadow is broken (since the light group SSBO change)

--- src/Headers/Build.h
+++ src/Headers/Build.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define Lumo_BuildNumber 548
+#define Lumo_BuildNumber 551
 #define Lumo_MinorNumber 2
 #define Lumo_MajorNumber 0
-#define Lumo_BuildId "0.2.548"
+#define Lumo_BuildId "0.2.551"
--- src/Modules/Renderers/Pass/DeferredRenderer_Pass.cpp
+++ src/Modules/Renderers/Pass/DeferredRenderer_Pass.cpp
@@ -283,9 +283,24 @@ void main()
 {
 	fragColor = vec4(0);
 	
+	if (use_sampler_diffuse > 0.5)
+	{
+		fragColor += texture(diffuse_map_sampler, v_uv);
+	}
+
+	if (use_sampler_specular > 0.5)
+	{
+		fragColor += texture(specular_map_sampler, v_uv);
+	}
+
+	if (use_sampler_attenuation > 0.5)
+	{
+		fragColor *= texture(attenuation_map_sampler, v_uv);
+	}
+
 	if (use_sampler_albedo > 0.5)
 	{
-		fragColor = texture(albedo_map_sampler, v_uv);
+		fragColor *= texture(albedo_map_sampler, v_uv);
 	}
 	
 	if (use_sampler_shadow > 0.5)
@@ -308,32 +323,6 @@ void main()
 			discard; // a faire en dernier
 		}
 	}
-
-	/*
-	vec3 pos = texture(gPosition, TexCoords).rgb;
-    vec3 normal = texture(gNormal, TexCoords).rgb;
-    vec3 color = texture(gAlbedo, TexCoords).rgb;
-    float AmbientOcclusion = texture(ssao, TexCoords).r;
-    
-    // blinn-phong (in view-space)
-    vec3 ambient = vec3(0.3 * Diffuse * AmbientOcclusion); // here we add occlusion factor
-    vec3 lighting  = ambient; 
-    vec3 viewDir  = normalize(-FragPos); // viewpos is (0.0.0) in view-space
-    // diffuse
-    vec3 lightDir = normalize(light.Position - FragPos);
-    vec3 diffuse = max(dot(Normal, lightDir), 0.0) * Diffuse * light.Color;
-    // specular
-    vec3 halfwayDir = normalize(lightDir + viewDir);  
-    float spec = pow(max(dot(Normal, halfwayDir), 0.0), 8.0);
-    vec3 specular = light.Color * spec;
-    // attenuation
-    float dist = length(light.Position - FragPos);
-    float attenuation = 1.0 / (1.0 + light.Linear * dist + light.Quadratic * dist * dist);
-    diffuse  *= attenuation;
-    specular *= attenuation;
-    lighting += diffuse + specular;
-    FragColor = vec4(lighting, 1.0);
-	*/
 }
 )";
 }
