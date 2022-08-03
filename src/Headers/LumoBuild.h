d1c6c80b208af5929f6c266a75321871c0bb048c
aiekick <aiekick@funparadigm.com>
2022-08-03 13:27:31 +0200

    -

--- src/Headers/LumoBuild.h
+++ src/Headers/LumoBuild.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define Lumo_BuildNumber 357
+#define Lumo_BuildNumber 358
 #define Lumo_MinorNumber 3
 #define Lumo_MajorNumber 0
-#define Lumo_BuildId "0.3.357"
+#define Lumo_BuildId "0.3.358"
