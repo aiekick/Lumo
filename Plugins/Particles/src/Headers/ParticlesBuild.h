b56e1a98d3ea84d99747f2afc846c9a748460f32
aiekick <aiekick@funparadigm.com>
2022-08-15 00:33:54 +0200

    -

--- Plugins/Particles/src/Headers/ParticlesBuild.h
+++ Plugins/Particles/src/Headers/ParticlesBuild.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define Particles_BuildNumber 312
+#define Particles_BuildNumber 313
 #define Particles_MinorNumber 0
 #define Particles_MajorNumber 0
-#define Particles_BuildId "0.0.312"
+#define Particles_BuildId "0.0.313"
--- libs/Common/src/Headers/CommonBuild.h
+++ libs/Common/src/Headers/CommonBuild.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define Common_BuildNumber 756
+#define Common_BuildNumber 757
 #define Common_MinorNumber 1
 #define Common_MajorNumber 0
-#define Common_BuildId "0.1.756"
+#define Common_BuildId "0.1.757"
