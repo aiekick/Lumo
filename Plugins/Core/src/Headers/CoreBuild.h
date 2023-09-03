2fe002a7c709148d499517cf26c2036caedbe007
aiekick <aiekick@funparadigm.com>
2023-09-04 00:41:54 +0200

    -

--- Plugins/Core/src/Headers/CoreBuild.h
+++ Plugins/Core/src/Headers/CoreBuild.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define Core_Prefix "Core"
-#define Core_BuildNumber 71
+#define Core_BuildNumber 73
 #define Core_MinorNumber 4
 #define Core_MajorNumber 0
-#define Core_BuildId "0.4.71"
+#define Core_BuildId "0.4.73"
