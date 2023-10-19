fa507b6b4c9c5a75ea88e716f1e0b5f09303230e
aiekick <aiekick@funparadigm.com>
2023-10-19 22:35:01 +0200

    -

--- Plugins/Core/src/Modules/Misc/GridModule.cpp
+++ Plugins/Core/src/Modules/Misc/GridModule.cpp
@@ -35,193 +35,159 @@ using namespace GaiApi;
 //// STATIC //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////
 
-std::shared_ptr<GridModule> GridModule::Create(GaiApi::VulkanCorePtr vVulkanCorePtr)
-{
-	if (!vVulkanCorePtr) return nullptr;
-	auto res = std::make_shared<GridModule>(vVulkanCorePtr);
-	res->m_This = res;
-	if (!res->Init())
-	{
-		res.reset();
-	}
-	return res;
+std::shared_ptr<GridModule> GridModule::Create(GaiApi::VulkanCorePtr vVulkanCorePtr) {
+    if (!vVulkanCorePtr)
+        return nullptr;
+    auto res = std::make_shared<GridModule>(vVulkanCorePtr);
+    res->m_This = res;
+    if (!res->Init()) {
+        res.reset();
+    }
+    return res;
 }
 
 //////////////////////////////////////////////////////////////
 //// CTOR / DTOR /////////////////////////////////////////////
 //////////////////////////////////////////////////////////////
 
-GridModule::GridModule(GaiApi::VulkanCorePtr vVulkanCorePtr)
-	: TaskRenderer(vVulkanCorePtr)
-{
-	m_SceneShaderPassPtr = SceneShaderPass::Create();
-}
+GridModule::GridModule(GaiApi::VulkanCorePtr vVulkanCorePtr) : TaskRenderer(vVulkanCorePtr) { m_SceneShaderPassPtr = SceneShaderPass::Create(); }
 
-GridModule::~GridModule()
-{
-	Unit();
+GridModule::~GridModule() {
+    Unit();
 
-	m_SceneShaderPassPtr.reset();
+    m_SceneShaderPassPtr.reset();
 }
 
 //////////////////////////////////////////////////////////////
 //// INIT ////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////
 
-bool GridModule::Init()
-{
-	ZoneScoped;
+bool GridModule::Init() {
+    ZoneScoped;
 
-	ct::uvec2 map_size = 512;
+    ct::uvec2 map_size = 512;
 
-	m_Loaded = true;
+    m_Loaded = true;
 
-	if (TaskRenderer::InitPixel(map_size))
-	{
-		m_GridModule_Vertex_Pass_Ptr = std::make_shared<GridModule_Vertex_Pass>(m_VulkanCorePtr);
-		if (m_GridModule_Vertex_Pass_Ptr)
-		{
-			if (m_GridModule_Vertex_Pass_Ptr->InitPixel(map_size, 1U, true, true, 0.0f, 
-				false, false, vk::Format::eR32G32B32A32Sfloat, vk::SampleCountFlagBits::e2))
-			{
-				AddGenericPass(m_GridModule_Vertex_Pass_Ptr);
-				m_SceneShaderPassPtr->Add(m_GridModule_Vertex_Pass_Ptr);
-				m_Loaded = true;
-			}
-		}
-	}
+    if (TaskRenderer::InitPixel(map_size)) {
+        m_GridModule_Vertex_Pass_Ptr = std::make_shared<GridModule_Vertex_Pass>(m_VulkanCorePtr);
+        if (m_GridModule_Vertex_Pass_Ptr) {
+            if (m_GridModule_Vertex_Pass_Ptr->InitPixel(map_size, 1U, true, true, 0.0f, false, false, vk::Format::eR32G32B32A32Sfloat, vk::SampleCountFlagBits::e2)) {
+                AddGenericPass(m_GridModule_Vertex_Pass_Ptr);
+                m_SceneShaderPassPtr->Add(m_GridModule_Vertex_Pass_Ptr);
+                m_Loaded = true;
+            }
+        }
+    }
 
-	return m_Loaded;
+    return m_Loaded;
 }
 
 //////////////////////////////////////////////////////////////
 //// PUBLIC //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////
 
-bool GridModule::ExecuteAllTime(const uint32_t& vCurrentFrame, vk::CommandBuffer* vCmd, BaseNodeState* vBaseNodeState)
-{
-	ZoneScoped;
+bool GridModule::ExecuteAllTime(const uint32_t& vCurrentFrame, vk::CommandBuffer* vCmd, BaseNodeState* vBaseNodeState) {
+    ZoneScoped;
 
-	TaskRenderer::Render("Grid Module", vCmd);
-	
-	return true;
+    TaskRenderer::Render("Grid Module", vCmd);
+
+    return true;
 }
 
-void GridModule::NeedResizeByResizeEvent(ct::ivec2* vNewSize, const uint32_t* vCountColorBuffers)
-{
-	TaskRenderer::NeedResizeByResizeEvent(vNewSize, vCountColorBuffers);
-}
+void GridModule::NeedResizeByResizeEvent(ct::ivec2* vNewSize, const uint32_t* vCountColorBuffers) { TaskRenderer::NeedResizeByResizeEvent(vNewSize, vCountColorBuffers); }
 
-bool GridModule::DrawWidgets(const uint32_t& vCurrentFrame, ImGuiContext* vContextPtr, const std::string& vUserDatas)
-{
-	assert(vContextPtr); ImGui::SetCurrentContext(vContextPtr);
+bool GridModule::DrawWidgets(const uint32_t& vCurrentFrame, ImGuiContext* vContextPtr, const std::string& vUserDatas) {
+    assert(vContextPtr);
+    ImGui::SetCurrentContext(vContextPtr);
 
-	ZoneScoped;
+    ZoneScoped;
 
-	if (IsTheGoodFrame(vCurrentFrame))
-	{
-		if (ImGui::CollapsingHeader_CheckBox("Grid/Axis Module", -1.0f, true, true, &m_CanWeRender))
-		{
-			if (m_GridModule_Vertex_Pass_Ptr)
-			{
-				return m_GridModule_Vertex_Pass_Ptr->DrawWidgets(vCurrentFrame, vContextPtr, vUserDatas);
-			}
-		}
-	}
+    if (IsTheGoodFrame(vCurrentFrame)) {
+        if (ImGui::CollapsingHeader_CheckBox("Grid/Axis Module", -1.0f, true, true, &m_CanWeRender)) {
+            if (m_GridModule_Vertex_Pass_Ptr) {
+                return m_GridModule_Vertex_Pass_Ptr->DrawWidgets(vCurrentFrame, vContextPtr, vUserDatas);
+            }
+        }
+    }
 
-	return false;
-}
-
-bool GridModule::DrawOverlays(const uint32_t& vCurrentFrame, const ImRect& vRect, ImGuiContext* vContextPtr, const std::string& vUserDatas)
-{
-	assert(vContextPtr); ImGui::SetCurrentContext(vContextPtr);
-
-	if (m_LastExecutedFrame == vCurrentFrame)
-	{
-
-	}
     return false;
 }
 
-bool GridModule::DrawDialogsAndPopups(const uint32_t& vCurrentFrame, const ImVec2& vMaxSize, ImGuiContext* vContextPtr, const std::string& vUserDatas)
-{
-	assert(vContextPtr); ImGui::SetCurrentContext(vContextPtr);
+bool GridModule::DrawOverlays(const uint32_t& vCurrentFrame, const ImRect& vRect, ImGuiContext* vContextPtr, const std::string& vUserDatas) {
+    assert(vContextPtr);
+    ImGui::SetCurrentContext(vContextPtr);
 
-	if (m_LastExecutedFrame == vCurrentFrame)
-	{
-
-	}
+    if (m_LastExecutedFrame == vCurrentFrame) {
+    }
     return false;
 }
 
-vk::DescriptorImageInfo* GridModule::GetDescriptorImageInfo(const uint32_t& vBindingPoint, ct::fvec2* vOutSize)
-{
-	if (m_GridModule_Vertex_Pass_Ptr)
-	{
-		return m_GridModule_Vertex_Pass_Ptr->GetDescriptorImageInfo(vBindingPoint, vOutSize);
-	}
+bool GridModule::DrawDialogsAndPopups(const uint32_t& vCurrentFrame, const ImVec2& vMaxSize, ImGuiContext* vContextPtr, const std::string& vUserDatas) {
+    assert(vContextPtr);
+    ImGui::SetCurrentContext(vContextPtr);
 
-	return nullptr;
+    if (m_LastExecutedFrame == vCurrentFrame) {
+    }
+    return false;
 }
 
-SceneShaderPassWeak GridModule::GetShaderPasses(const uint32_t& vSlotID)
-{
-	return m_SceneShaderPassPtr;
+vk::DescriptorImageInfo* GridModule::GetDescriptorImageInfo(const uint32_t& vBindingPoint, ct::fvec2* vOutSize) {
+    if (m_GridModule_Vertex_Pass_Ptr) {
+        return m_GridModule_Vertex_Pass_Ptr->GetDescriptorImageInfo(vBindingPoint, vOutSize);
+    }
+
+    return nullptr;
 }
+
+SceneShaderPassWeak GridModule::GetShaderPasses(const uint32_t& vSlotID) { return m_SceneShaderPassPtr; }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //// CONFIGURATION /////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-std::string GridModule::getXml(const std::string& vOffset, const std::string& vUserDatas)
-{
-	std::string str;
+std::string GridModule::getXml(const std::string& vOffset, const std::string& vUserDatas) {
+    std::string str;
 
-	str += vOffset + "<grid_module>\n";
+    str += vOffset + "<grid_module>\n";
 
-	str += vOffset + "\t<can_we_render>" + (m_CanWeRender ? "true" : "false") + "</can_we_render>\n";
+    str += vOffset + "\t<can_we_render>" + (m_CanWeRender ? "true" : "false") + "</can_we_render>\n";
 
-	for (auto pass : m_ShaderPasses)
-	{
-		auto pass_ptr = pass.lock();
-		if (pass_ptr)
-		{
-			str += pass_ptr->getXml(vOffset + "\t", vUserDatas);
-		}
-	}
+    for (auto pass : m_ShaderPasses) {
+        auto pass_ptr = pass.lock();
+        if (pass_ptr) {
+            str += pass_ptr->getXml(vOffset + "\t", vUserDatas);
+        }
+    }
 
-	str += vOffset + "</grid_module>\n";
+    str += vOffset + "</grid_module>\n";
 
-	return str;
+    return str;
 }
 
-bool GridModule::setFromXml(tinyxml2::XMLElement* vElem, tinyxml2::XMLElement* vParent, const std::string& vUserDatas)
-{
-	// The value of this child identifies the name of this element
-	std::string strName;
-	std::string strValue;
-	std::string strParentName;
+bool GridModule::setFromXml(tinyxml2::XMLElement* vElem, tinyxml2::XMLElement* vParent, const std::string& vUserDatas) {
+    // The value of this child identifies the name of this element
+    std::string strName;
+    std::string strValue;
+    std::string strParentName;
 
-	strName = vElem->Value();
-	if (vElem->GetText())
-		strValue = vElem->GetText();
-	if (vParent != nullptr)
-		strParentName = vParent->Value();
+    strName = vElem->Value();
+    if (vElem->GetText())
+        strValue = vElem->GetText();
+    if (vParent != nullptr)
+        strParentName = vParent->Value();
 
-	if (strParentName == "grid_module")
-	{
-		if (strName == "can_we_render")
-			m_CanWeRender = ct::ivariant(strValue).GetB();
-	}
+    if (strParentName == "grid_module") {
+        if (strName == "can_we_render")
+            m_CanWeRender = ct::ivariant(strValue).GetB();
+    }
 
-	for (auto pass : m_ShaderPasses)
-	{
-		auto pass_ptr = pass.lock();
-		if (pass_ptr)
-		{
-			pass_ptr->setFromXml(vElem, vParent, vUserDatas);
-		}
-	}
+    for (auto pass : m_ShaderPasses) {
+        auto pass_ptr = pass.lock();
+        if (pass_ptr) {
+            pass_ptr->setFromXml(vElem, vParent, vUserDatas);
+        }
+    }
 
-	return true;
+    return true;
 }
