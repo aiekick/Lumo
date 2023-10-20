40a944b6edbd689c6010988da0b9cad0ffb27781
aiekick <aiekick@funparadigm.com>
2023-10-21 01:22:55 +0200

    fix cpack generation for unix

--- Plugins/MeshGen/src/Headers/MeshGenBuild.h
+++ Plugins/MeshGen/src/Headers/MeshGenBuild.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define MeshGen_Prefix "MeshGen"
-#define MeshGen_BuildNumber 307
+#define MeshGen_BuildNumber 309
 #define MeshGen_MinorNumber 0
 #define MeshGen_MajorNumber 0
-#define MeshGen_BuildId "0.0.307"
+#define MeshGen_BuildId "0.0.309"
--- Plugins/PlanetSystem/src/Headers/PlanetSystemBuild.h
+++ Plugins/PlanetSystem/src/Headers/PlanetSystemBuild.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define PlanetSystem_Prefix "PlanetSystem"
-#define PlanetSystem_BuildNumber 128
+#define PlanetSystem_BuildNumber 130
 #define PlanetSystem_MinorNumber 0
 #define PlanetSystem_MajorNumber 0
-#define PlanetSystem_BuildId "0.0.128"
+#define PlanetSystem_BuildId "0.0.130"
