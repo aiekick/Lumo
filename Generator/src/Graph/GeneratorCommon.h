35ae051dc1836de2ab8f7dd895015daead62b528
aiekick <aiekick@funparadigm.com>
2023-10-21 16:44:19 +0200

    removal of old code generator app

--- Generator/src/Graph/GeneratorCommon.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class GeneratorNodeSlotDatas
-{
-public:
-	uint32_t editorSlotTypeIndex = 0U;		// like variable slot
-	uint32_t editorSlotSubTypeIndex = 0U;	// like sub type of variable slot
-};
--- Generator/src/Headers/LumoCodeGeneratorBuild.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define LumoCodeGenerator_Prefix "LumoCodeGenerator"
-#define LumoCodeGenerator_BuildNumber 626
-#define LumoCodeGenerator_MinorNumber 0
-#define LumoCodeGenerator_MajorNumber 0
-#define LumoCodeGenerator_BuildId "0.0.626"
