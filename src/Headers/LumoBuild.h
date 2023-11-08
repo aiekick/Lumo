1fd9fd1dbc63bd0bd5c2bb027e82bf3c9fc6ab54
aiekick <aiekick@funparadigm.com>
2023-11-08 22:48:36 +0100

    -

--- src/Headers/LumoBuild.h
+++ src/Headers/LumoBuild.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define Lumo_Prefix "Lumo"
-#define Lumo_BuildNumber 5325
+#define Lumo_BuildNumber 5328
 #define Lumo_MinorNumber 0
 #define Lumo_MajorNumber 0
-#define Lumo_BuildId "0.0.5325"
+#define Lumo_BuildId "0.0.5328"
