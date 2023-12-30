a544f93952f79884d898fb278ad84242534f146a
aiekick <aiekick@funparadigm.com>
2023-12-30 15:52:51 +0100

    -

--- Plugins/CodeGenerator/src/Headers/CodeGeneratorBuild.h
+++ Plugins/CodeGenerator/src/Headers/CodeGeneratorBuild.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define CodeGenerator_Prefix "CodeGenerator"
-#define CodeGenerator_BuildNumber 734
+#define CodeGenerator_BuildNumber 735
 #define CodeGenerator_MinorNumber 0
 #define CodeGenerator_MajorNumber 0
-#define CodeGenerator_BuildId "0.0.734"
+#define CodeGenerator_BuildId "0.0.735"
